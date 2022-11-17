e18f0a647f7d61c073c238049f626afd81d8e91b
mengxianglinx <mengxianglinx@users.noreply.github.com>
2022-11-17 07:14:49 +0000

    Add initial `aarch64` crates for basic boot-up (#688)
    * This marks the beginning of  our effort to port Theseus to `aarch64`.
      Currently, all components live in a separate workspace in the `aarch64/` directory
      and are excluded from the top-level workspace build of Theseus.
      We will gradually merge these components into the main Theseus workspace,
      but for now it acts as a staging directory for `aarch64`-specific development.
    
    * This includes a basic serial port / UART-based logger and a basic
      `nano_core` that simply takes over from a UEFI bootloader.
    
    * To build and run, see the instructions in `aarch64/README`.
    
    * Depends on newer versions of  `uefi` and `uefi-services` that include
      our minor PRs to enable logging within those crates.
    
    Co-authored-by: Kevin Boos <1139460+kevinaboos@users.noreply.github.com> 8e1b3734b31f2807a5801520c3ec422524b5602a

--- doc/mouse/sidebar-items.js
+++ doc/mouse/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["handle_mouse_input","return a Mouse Event according to the data"],["init","Initialize the mouse driver."],["mouse_to_print","print the mouse actions"]]});
+window.SIDEBAR_ITEMS = {"fn":[["init","Initialize the PS/2 mouse driver and register its interrupt handler."]]};
